b749793b3352d504d21533b835169acc731ae1df
bohdan-vasylenko <bohdan.vasylenko@gmail.com>
2021-12-10 17:58:29 +0100

    graphql init

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import {MikroORM} from '@mikro-orm/core';
-import {__prod__} from "./constants";
-import {Post} from "./entities/Post";
+// import {__prod__} from "./constants";
+// import {Post} from "./entities/Post";
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => console.log('Server started at port: 4000'))
+
+
     // const post = orm.em.create(Post, {title: 'my first post'})
     // await orm.em.persistAndFlush(post)
     // const posts = await orm.em.find(Post, {});
